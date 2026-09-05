db3de2a1abd4d04aad5570f8d8fba838eb9616cb
lhiepnhi-web <lhiepnhi@gmail.com>
2026-09-05 13:39:37 +0700

    Add CORE26 configuration list to config.py

---
+++ src/config.py
@@ -0,0 +1,8 @@
+ CORE26 = [
+    "THD", "ACG", "VJC", "PLX", "HAG",
+    "DGW", "DPM", "KDC", "CEO",
+    "SSI", "DBC", "VCI", "NVL", "VPB",
+    "VTP", "LPB", "BFC", "SHS", "VIB",
+    "VND", "MBS", "VGC", "PVD", "VSC",
+    "NAF", "MCH"
+ ]
